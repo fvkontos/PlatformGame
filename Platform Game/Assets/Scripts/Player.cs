dba2f165549510690ce38f473ce0610b7b17bfad
Filippos-Vladimiros Kontos <fvkontos@hotmail.com>
2021-05-05 13:49:20 +0300

    First Lesson
    Creation of platforms , player , collectables . Player can move , jump and double jump , has gravity and we display how many coins we have collected .

---
+++ Platform Game/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private float gravity = 1.0f;
+    private float jumpHeight = 15.0f;
+    private float dyTemp;
+
+    private int coins;
+
+    private bool canDoubleJump = false;
+
+    private CharacterController controller;
+
+    private UIManager uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        direction *=  speed;
+
+        if(!controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && canDoubleJump)
+            {
+                dyTemp += jumpHeight;
+                canDoubleJump = false;
+            }
+            
+            dyTemp -= gravity;
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                dyTemp = jumpHeight;
+                canDoubleJump = true;
+            }
+        }
+
+        direction.y = dyTemp;
+
+        controller.Move(direction  * Time.deltaTime);
+    }
+
+    public void AddCoin()
+    {
+        coins++;
+
+        if(uiManager != null)
+        {
+            uiManager.CoinDisplay(coins);
+        }
+    }
+}
---
+++ Platform Game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CoinDisplay(int coins)
+    {
+        coinText.text = "Coins : " + coins;
+    }
+}
